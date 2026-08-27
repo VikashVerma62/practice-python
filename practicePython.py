d851f95b512698e241aaae678e052c60f6e23c42
VikashVerma62 <vikashverma626506@gmail.com>
2026-08-27 11:43:35 +0530

    update

---
+++ practicePython.py
@@ -0,0 +1,74 @@
+# import keyword
+# key=keyword.kwlist
+# print(key)
+
+# import keyword
+# key=keyword.softkwlist
+# print(key)
+
+# import string
+# pun=string.punctuation
+# print(pun)
+
+# check mutable
+# b=[2,3,4]
+# print(id(b))
+# b.append(90)
+# print(id(b))
+
+# check immutable
+# a=11
+# print(id(a))
+# a=a+8
+# print(id(a))
+
+# arthmetic operater
+a=10
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# comparision operater
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
+# assingment operater
+c=a
+a+=10
+print(a)
+b-=2
+print(b)
+b=b*2
+print(b)
+b=b/2
+print(b)
+
+b=b//2
+print(b)
+
+b=b%2
+print(b)
+
+b=b**2
+print(b)
+
+# logical operater
+
+if a>b or b>a:
+    print("true")
+
+
+a="hello world"
+print(a.upper())
+
