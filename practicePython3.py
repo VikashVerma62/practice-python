ac62cb1229998f661fcd2ce0ea5afb812dd54bf9
VikashVerma62 <vikashverma626506@gmail.com>
2026-08-28 18:30:00 +0530

    update

---
+++ practicePython3.py
@@ -0,0 +1,93 @@
+
+#!Q1. Take a number as input. Print "Even" if it is even, otherwise print "Odd".
+# n=int(input("Enter a number "))
+# if n%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#!Q2. Take a person's age as input. Print "Adult" if age is 18 or above, otherwise print "Minor".
+# age=int(input("Enter the age "))
+# if age>=18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+
+#!Q3. Take two numbers as input. Print the larger one. If both are equal, print "Both are equal".
+# n1=int(input("Enter first number "))
+# n2=int(input("Enter second number "))
+
+# if n1>n2:
+#     print(n1,"is greater")
+# else:
+#     print(n2,"is greater")
+
+
+#!Q4. Take a temperature as input. If it is above 40, print "Very Hot", otherwise print "Normal"
+
+# temp=float(input("Enter the tempearature"))
+# if temp>=40:
+#     print("Very Hot")
+# else:
+#     print("Normal")
+
+
+
+#!Q5. Take a password as input. If it matches "python123", print "Access Granted", otherwise print
+#! "Access Denied".
+
+# n=input("Enter the password ")
+# if n=="python123":
+#     print("Access Granted ")
+# else:
+#     print("Access Denied")
+
+
+#!Q6. Take a number as input. If it is positive, print "Positive", otherwise print "Not Positive".
+# n=int(input("Enter the number "))
+# if n>0:
+#     print("positive")
+# else:
+#     print("negative")
+
+
+#!Q7. Take balance and withdrawal amount as input. If withdrawal amount is less than or equal to
+# balance, print the remaining balance. Otherwise print "Insufficient Balance". 
+
+# balance=int(input("Enter balance "))
+# withdwal=int(input("Enter withdrawal "))
+
+# if withdwal<=balance:
+#     print("The remaining balance",balance-withdwal)
+# else:
+#     print("insufficient balance")
+
+
+#!Q8. Take a year as input. If it is divisible by 2, print "Even Year", otherwise print "Odd Year".
+# num=int(input("Enter number"))
+# if num%2==0:
+#     print("Even year")
+# else:
+#     print("Odd leap year") 
+
+
+
+#!Q9. Take a student's marks as input. If marks are 40 or above print "Pass" and also print the marks
+# marks=int(input("Enter the marks "))
+# if marks>=40:
+#     print("pass")
+# else:
+#     print("fail")
+#     print("marks need to pass ",40-marks)
+
+
+#Q10. Take two numbers as input. If first number is divisible by second number, print "Divisible" and
+# print the quotient. Otherwise print "Not Divisible".
+
+# n1=int(input("Enter first number "))
+# n2=int(input("Enter second number "))
+# if n1%n2==0:
+#     print(n1,"is divisible by ",n2)
+# else:
+#     print(n1,"is not divisible by",n2)
